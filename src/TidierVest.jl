fbb92a71dbfb45def3402464f71198a29bcd3afc
José Díaz <josediazrrnn@gmail.com>
2025-04-22 21:55:48 +0200

    exported gumbo types

--- src/TidierVest.jl
+++ src/TidierVest.jl
@@ -1,11 +1,15 @@
 module TidierVest
 
     using HTTP, Cascadia, Gumbo, DataFrames
-
+    
+    ## TidierVest functions
     export parse_html, read_html, html_elements, html_attrs
     export html_text, html_text2, html_text3
     export html_table
     export minimal_html
+
+    ## Gumbo types
+    export HTMLDocument, HTMLNode, HTMLElement, HTMLText
 
     include("resources.jl")
     include("utils.jl")
